cccb4bccc452043504b4e420cdb3fd19dab1f08a
Colin Meinke <hello@colinmeinke.com>
2016-04-21 21:13:13 -0500

    test(boundingBox): add test for use with shape array

--- test/boundingBox.js
+++ test/boundingBox.js
@@ -3,7 +3,7 @@ import expect from 'expect';
 import boundingBox from '../src/boundingBox';
 
 describe( 'boundingBox', () => {
-  it( 'should return correct coordinates', () => {
+  it( 'should return correct coordinates from shape', () => {
     const points = [
       { x: 0, y: 0, moveTo: true },
       { x: 100, y: 0 },
@@ -17,6 +17,35 @@ describe( 'boundingBox', () => {
       center: { x: 50, y: 50 },
       left: 0,
       right: 100,
+      top: 0,
+    };
+
+    expect( boundingBox( points )).toEqual( expectedCoordinates );
+  });
+
+  it( 'should return correct coordinates from shape array', () => {
+    const points = [
+      [
+        { x: 0, y: 0, moveTo: true },
+        { x: 100, y: 0 },
+        { x: 100, y: 100 },
+        { x: 0, y: 100 },
+        { x: 0, y: 0 },
+      ],
+      [
+        { x: 50, y: 50, moveTo: true },
+        { x: 150, y: 50 },
+        { x: 150, y: 150 },
+        { x: 50, y: 150 },
+        { x: 50, y: 50 },
+      ],
+    ];
+
+    const expectedCoordinates = {
+      bottom: 150,
+      center: { x: 75, y: 75 },
+      left: 0,
+      right: 150,
       top: 0,
     };
 
