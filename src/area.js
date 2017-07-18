9519c663439ee267f1dda75b90557bb028454c64
dalisoft <dalisoft@mail.ru>
2017-07-18 23:24:57 +0500

    Update area.js

--- src/area.js
+++ src/area.js
@@ -1,6 +1,6 @@
 import { applyFuncToShapes } from './helpers';
 
-const areaPoints = (polygon) => {
+const areaPoints = polygon => {
   return polygon.reduce((area, {x:x,y:y}, i) => {
 	const {x:x1,y:y1} = i === 0 ? polygon[polygon.length - 1] : polygon[i - 1];
 	area += (x1 + x) * (y1 - y)
